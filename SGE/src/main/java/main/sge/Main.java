aa4056f8dade5565e6fc6f5e404925c0cecabec5
jmoca <114067390+jmoca@users.noreply.github.com>
2023-12-05 14:09:42 +0100

    +1

---
+++ SGE/src/main/java/main/sge/Main.java
@@ -0,0 +1,38 @@
+package main.sge;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Cargar la vista desde el archivo FXML
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = loader.load();
+
+        // Configurar el controlador si es necesario
+        HelloController controller = loader.getController();
+        // Puedes realizar configuraciones adicionales en el controlador si es necesario
+
+        // Configurar la escena
+        Scene scene = new Scene(root);
+
+        // Configurar el escenario (ventana)
+        stage.setTitle("SGE Application");
+        stage.setScene(scene);
+
+        // Mostrar la ventana
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
+
--- SGE/src/main/java/module-info.java
+++ SGE/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module main.sge {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens main.sge to javafx.fxml;
